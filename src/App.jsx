487346a24bb8d480a4385359b97304d8d0e033d1
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-11-13 14:29:50 -0500

    sign in feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm'
+import SigninForm from './components/SigninForm/SigninForm'
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -8,7 +13,16 @@ const App = () => {
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+
+        <Route path='/signup' element={<SignupForm setUser={setUser}/>} /> 
+        <Route path='/signin' element={<SigninForm setUser={setUser}/>} /> 
+      </Routes>
     </>
   )
 }
---
+++ src/service/authService.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+
+const signup = async (formData) => {
+    try {
+const res = await axios.post(`${BACKEND_URL}/users/signup`,formData)
+
+if (res.err) {
+    throw new Error(res.err)
+}
+
+return res.data;
+    } catch (err) {
+        console.log(err)
+        throw err;
+    }
+}
+
+const signin = async (user) => {
+    try {
+      const res = await axios.post(`${BACKEND_URL}/users/signin`,user)
+      if (res.data.error) {
+        throw new Error(res.data.error);
+      }
+  
+      if (res.data.token) {
+        const user = JSON.parse(atob(res.data.token.split('.')[1]));
+        return user;
+      }
+
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+
+export {
+    signup,
+    signin
+}
